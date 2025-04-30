866c458e3fd0a2757f80a5426bd1d669bfa251ec
Adrian Malaguti <adrianma78@hotmail.com>
2025-04-30 16:47:00 -0300

    show products

--- app.js
+++ app.js
@@ -7,27 +7,48 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files (css, js, img files)
+app.use(express.static(path.join(__dirname, 'public')));
+
 // handlebars
 const handlebars = require('express-handlebars');
 app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
+// Import productsManager
+const productsManager = require('./products/productsManager.js');
 
 // Routes
 const realTimeProductsRouter = require('./routes/realTimeProducts.router.js');
+
+
+// Home shows all products
+app.get('/', async (req, res) => {
+    try {
+        const productos = await productsManager.getProducts();
+        res.render("home", { 
+            title: "Entrega 2",
+            productos: productos
+        });
+    } catch (error) {
+        console.error('Error fetching products:', error.message);
+        res.status(500).send('Error loading products');
+    }
+});
+
+app.get('/custom_layout', (req, res) => {
+    res.render("home", { 
+        layout: 'custom-layout',
+        title: "Custom Layout Test",
+    });
+});
+
+
+// /api/realtimeproducts uses websockets
 app.use('/api/realtimeproducts', realTimeProductsRouter);
 
 
-
-app.get('/', (req, res) => {
-  //res.send('Hello World !!!')
-	res.render("home", { 
-    layout: 'custom-layout',
-		title: "Entrega 2",
-	});
-})
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`)
+});
--- routes/realTimeProducts.router.js
+++ routes/realTimeProducts.router.js
@@ -5,15 +5,6 @@ const productsManager = require('../products/productsManager.js');
 
 
 // GET /api/realtimeproducts
-router.get('/', async (req, res) => {
-    try {
-        const products = await productsManager.getProducts();
-        console.log("Retrieving products...")
-        res.json(products);
-    } catch (error) {
-        console.error('Error fetching products:', error.message);
-        res.status(500).json({ error: 'Error fetching products' });
-    }
-});
+router.get('/', async (req, res) => {res.status(200).send('Websockets');});
 
 module.exports = router;
